1dea1bc77c5e735e8562a355f57eef9fbe44c520
Keven <kevenssouza1@gmail.com>
2023-02-08 18:58:00 -0300

    Add Header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <img
+          src="https://github.com/kevensouzz.png"
+          alt="Foto de Keven Souza"
+        />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
